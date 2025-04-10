c2f43baee1f571f1ce9444f423d91e28715b9e03
satya-git07 <sbappadala@softility.com>
2025-04-10 15:09:50 +0530

    Create main.tf

---
+++ terraform/modules/bucket/main.tf
@@ -0,0 +1,16 @@
+
+# Create GCP Bucket
+resource "google_storage_bucket" "tf_state_bucket" {
+  name          = "return-of-the-dragon"  # Replace with a unique bucket name
+  location      = "US"  # Replace with your desired bucket location
+  storage_class = "STANDARD"
+  uniform_bucket_level_access = true
+}
+
+# Backend Configuration for Terraform State
+terraform {
+  backend "gcs" {
+    bucket = "my-tf-state-bucket"  # Match the bucket name created above
+    prefix = "terraform/state"     # Path where the state file will be stored
+  }
+}
