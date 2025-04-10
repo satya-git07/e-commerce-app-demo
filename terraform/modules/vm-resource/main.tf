2340c84c78212c24b434be147404aaa6d0a24b70
sgandeplli <sgandepalli@softility.com>
2025-04-10 12:30:46 +0530

    Create main.tf

---
+++ terraform/modules/vm-resource/main.tf
@@ -0,0 +1,16 @@
+resource "google_compute_instance" "test1" {
+  name         = "naruto12"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-stream-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
