5f5ca98a85f75170ace63d0e17e063d00b006b5b
sgandeplli <sgandepalli@softility.com>
2025-04-10 13:41:25 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+provider "google" {
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+module "gcp_instance" {
+  source        = "./modules/gcp-instance"
+  instance_name = "naruto12"
+  machine_type  = "e2-micro"
+  image         = "centos-cloud/centos-stream-9"
+  network       = "default"
+  zone          = "us-central1-a"
+}
