67a1090db250e9ad7472629d35f4082605768455
sgandeplli <sgandepalli@softility.com>
2025-04-10 13:55:42 +0530

    Create variable.tf

---
+++ terraform/variable.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+  default = "sekhar-452813"
+}
+
+variable "region" {
+  description = "The region where the resources will be created"
+  type        = string
+  default = "us-central1"
+}
