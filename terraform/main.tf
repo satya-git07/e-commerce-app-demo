297ff5d13ca73d0b9aa77dcbfa26627ff91a601f
sgandeplli <sgandepalli@softility.com>
2025-04-10 13:53:39 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,7 @@ provider "google" {
   zone    = "us-central1-a"
 }
 
+#creating the vm-instances
 module "gcp_instance" {
   source        = "./modules/vm-resources"
   instance_name = "naruto12"
@@ -10,4 +11,20 @@ module "gcp_instance" {
   image         = "centos-cloud/centos-stream-9"
   network       = "default"
   zone          = "us-central1-a"
+}
+
+
+#creating the cluster
+module "gke_cluster" {
+  source = "./modules/gke-cluster"
+
+  cluster_name          = "my-gke-cluster"
+  region                = "us-central1"
+  node_count            = 3
+  node_machine_type     = "e2-medium"
+  node_disk_size        = 100
+  create_node_pool      = true  # This will create the custom node pool
+  node_pool_name        = "custom-node-pool"
+  node_pool_count       = 3
+  node_pool_machine_type = "e2-small"
 }
